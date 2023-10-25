864e39d933876903cabb1e8f748127a735e1352e
RonOvski1212 <245785gio@gmail.com>
2023-10-25 19:45:31 +0400

    optimisation

--- src/index.js
+++ src/index.js
@@ -1,43 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { legacy_createStore as createStore, bindActionCreators } from "redux";
+import { createRoot } from "react-dom/client";
+
+import { createStore } from "redux";
 import reducer from "./reducer";
-import * as actions from "./actions";
+import { Provider } from "react-redux";
+
+import App from "./components/App";
 
 const store = createStore(reducer);
 
-const { dispatch, subscribe, getState } = store;
-
-const update = () => {
-  document.getElementById("counter").textContent = getState().value;
-};
-
-subscribe(update);
-
-// const bindActionCreator =
-//   (creator, dispatch) =>
-//   (...args) => {
-//     dispatch(creator(...args));
-//   };
-
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-// const incDispatch = bindActionCreators(inc, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById("inc").addEventListener("click", inc);
-
-document.getElementById("dec").addEventListener("click", dec);
-
-document.getElementById("rnd").addEventListener("click", () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <></>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
