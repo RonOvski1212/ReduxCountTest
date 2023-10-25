9ba47232ce98699433c6f3315c482a81474e161e
RonOvski1212 <245785gio@gmail.com>
2023-10-25 20:46:23 +0400

    optimisation functional component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,5 @@
 import { connect } from "react-redux";
 import * as actions from "../actions";
-import { bindActionCreators } from "redux";
 
 const Counter = ({ counter, inc, dec, rnd }) => {
   return (
@@ -25,16 +24,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-  return {
-    inc,
-    dec,
-    rnd: () => {
-      const value = Math.floor(Math.random() * 10);
-      rnd(value);
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, actions)(Counter);
